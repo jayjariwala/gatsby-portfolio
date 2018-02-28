e48a36f36a6c4937a304a3a27f443bdbc3bfa1d5
jayjariwala <jay.jariwala@hotmail.com>
2018-02-28 18:03:23 -0500

    feature: mobile navigation

--- src/components/MobileNavigation/index.js
+++ src/components/MobileNavigation/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import brand from '../../assets/images/brand.png';
 
 const MobileNavigation = () => (
   <nav className="mobile-navbar">
     <div className="mobile-navbar__header">
+      <img src={brand} className="navbar__logo"/>
+      <i className="fas fa-bars"></i>
     </div>
-    <ul className="mobile-navbar__body">
-      <li>Projects</li>
-      <li>Blog</li>
-      <li>Tips and Tricks</li>
-    </ul>
+    <div className="mobile-navbar__links">
+      <ul>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </div>
 	</nav>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
+import profile_picture from '../assets/images/display-picture.png';
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="hero">
+    <div className="hero__overlay"/>
+    <section className="hero__content wrapper" >
+      <div className="hero__introduction ">
+        <h1 className="hero__intro-heading">Hi, I'm Jay Jariwala</h1>
+				<p className="hero__intro-subheading">Software Engineer</p>
+				<p className="hero__intro-paragraph">I build Full-stack Web applications with emphasis of writing clean and maintainable code. Hobbies are reading 📖 , coffee ☕ and 🤓 coding. I am currently open for Full-time positions.</p>
+				<button className="button">Get in Touch</button>
+				<button className="button button--green"><i className="fas fa-cloud-download-alt"></i> Resume</button>
+				<div className="social">
+					<i className="fab fa-twitter"></i>
+					<i className="fab fa-github"></i>
+					<i className="fab fa-linkedin"></i>
+					<i className="fab fa-medium"></i>
+				</div>
+      </div>
+      <div className="hero__display-image-container">
+        <img src={profile_picture} className="hero__display-picture" />
+      </div>
+    </section>
   </div>
 )
 
