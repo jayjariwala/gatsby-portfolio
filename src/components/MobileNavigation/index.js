fd1649b11451abccc51da16718a9cfd21b4cc645
jayjariwala <jay.jariwala@hotmail.com>
2018-02-27 20:00:13 -0500

    feature:frontend Site layout

---
+++ src/components/MobileNavigation/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const MobileNavigation = () => (
+  <nav className="navbar">
+    <div className="navbar__container">
+      <img className="js-brand navbar__logo" width="65px"/>
+      <div className="navbar__name-container">
+        <p>Jay</p>
+        <p>Jariwala</p>
+      </div>
+      <ul className="navbar__nav-items">
+        <li><a href="">Projects</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">About</a></li>
+        <p className="navbar__twitter-follow">
+          <a href="https://twitter.com/_deadlocked?ref_src=twsrc%5Etfw" className="twitter-follow-button">Follow @_deadlocked</a>
+          <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+        </p>
+        <div className="quote-ticker">
+          <button className="button">I <i className="fas fa-heart"></i> Quotes</button>
+          <div className="quote-ticker__wrapper">
+            <i className="fas fa-quote-left"></i>
+            <span className="quote-ticker__quote">
+            Don't cry because it's over, smile because it happened.
+            </span>
+            <i className="fas fa-quote-right"></i>
+          </div>
+        </div>
+      </ul>
+    </div>
+	</nav>
+)
+
+export default MobileNavigation;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import './index.css'
+import Navigation from '../components/Navigation'
+import '../styles/styles.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -14,16 +14,13 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+    <div className="layout">
+      <div className="layout__navigation">
+        <Navigation />
+      </div>
+      <div className="layout__content">
+        {children()}
+      </div>
     </div>
   </div>
 )
