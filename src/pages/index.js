c23d33969cbc69c806aa3d5552cd38c250c4b090
jayjariwala <jay.jariwala@hotmail.com>
2018-04-20 16:26:38 -0400

    link url added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import profile_picture from '../assets/images/display-picture.png';
+import resume from '../assets/rawfiles/jayjariwala_resume.pdf';
 
 const IndexPage = () => (
   <div className="hero">
@@ -10,13 +11,13 @@ const IndexPage = () => (
         <h1 className="hero__intro-heading">Hi, I'm Jay Jariwala</h1>
 				<p className="hero__intro-subheading">Software Engineer</p>
 				<p className="hero__intro-paragraph">I build Full-stack Web applications with emphasis of writing clean and maintainable code. Hobbies are reading 📖 , coffee ☕ and 🤓 coding. I am currently open for Full-time positions.</p>
-				<button className="button button--white button--round">Get in Touch</button>
-				<button className="button button--green button--round"><i className="fas fa-cloud-download-alt"></i> Resume</button>
+				<a href="mailto:jay.jariwala@hotmail.com?Subject=Hello" target="_top"><button className="button button--white button--round">Get in Touch</button></a>
+				<a href={resume} target="__blank"><button className="button button--green button--round"><i className="fas fa-cloud-download-alt"></i> Resume</button></a>
 				<div className="social">
-					<i className="fab fa-twitter"></i>
-					<i className="fab fa-github"></i>
-					<i className="fab fa-linkedin"></i>
-					<i className="fab fa-medium"></i>
+					<a href="https://twitter.com/_deadlocked" target="__blank"><i className="fab fa-twitter"></i></a>
+					<a href="https://github.com/jayjariwala" target="__blank"><i className="fab fa-github"></i></a>
+					<a href="https://www.linkedin.com/in/jay-jariwala/" target="__blank"><i className="fab fa-linkedin"></i></a>
+					<a href="https://www.facebook.com/jay.jariwala.5" target="__blank"><i className="fab fa-facebook"></i></a>
 				</div>
       </div>
       <div className="hero__display-image-container">
