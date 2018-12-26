dc810b514949fe0f401667ee9cb5530e98ddf3aa
jayjariwala <jay.jariwala@hotmail.com>
2018-12-26 00:45:02 -0500

    index page introduction change

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link';
 import profile_picture from '../assets/images/display-picture.png';
 import resume from '../assets/rawfiles/jayjariwala_resume.pdf';
 
+
 const IndexPage = () => (
   <div className="hero">
     <div className="hero__overlay"/>
@@ -10,7 +11,7 @@ const IndexPage = () => (
       <div className="hero__introduction ">
         <h1 className="hero__intro-heading" > Hi! <div className="hand_waving">👋 </div> I'm Jay Jariwala </h1>
 				<p className="hero__intro-subheading">Software Engineer</p>
-				<p className="hero__intro-paragraph">I build Full-stack Web applications with emphasis of writing clean and maintainable code. I have good command over JavaScript language and It's libraries. When not coding I enjoy outdoors 🚞 and 📸 photography. I am currently open for Full-time positions.</p>
+				<p className="hero__intro-paragraph">I build Full-stack Web applications with emphasis of writing clean and maintainable code. I have good command over JavaScript language and It's libraries. When not coding, I enjoy outdoors and photography. <br/><br/> Check out some of my <a href="#" className="inline-link" ><em>Projects</em></a> and <a href="#" className="inline-link"> <em>Photos</em> </a></p>
 				<a href="mailto:jay.jariwala@hotmail.com?Subject=Hello" target="_top"><button className="button button--white button--round">Get in Touch</button></a>
 				<a href={resume} target="__blank"><button className="button button--green button--round"><i className="fas fa-cloud-download-alt"></i> Resume</button></a>
 				<div className="social">
